ed9249b11f7f4ed0f23e0ec888395c24913ace35
Jaime <joriolgo@gmail.com>
2025-05-30 07:32:11 +0200

    fix: remove unused imports in about page - Remove AuthorLayout import that doesn't exist - Remove contentlayer imports that aren't being used

--- blog/app/about/page.tsx
+++ blog/app/about/page.tsx
@@ -1,7 +1,3 @@
-import { Authors, allAuthors } from 'contentlayer/generated'
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 import PhotoCarousel from '@/components/PhotoCarousel'
 import BioSection from '@/components/BioSection'
@@ -9,9 +5,6 @@ import BioSection from '@/components/BioSection'
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
-
   return (
     <>
       {/* Galería de fotos a pantalla completa */}
