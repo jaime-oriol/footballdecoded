10d09a608a9f9de21c922bf63bf33c4c79ba7cd1
Jaime <joriolgo@gmail.com>
2025-06-06 17:53:44 +0200

    Delete Fotmob

--- scrappers/__init__.py
+++ scrappers/__init__.py
@@ -4,12 +4,10 @@ __version__ = "1.8.7"
 
 __all__ = [
     "FBref",
-    "FotMob",
     "Understat",
     "WhoScored",
 ]
 
 from .fbref import FBref
-from .fotmob import FotMob
 from .understat import Understat
 from .whoscored import WhoScored
